2bd23529db0931b0bd2c3a66a743ed3b00248d6a
fabianriewe <fabianriewe00@gmail.com>
2019-12-22 19:27:52 +0100

    refactor: removed unused files

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,22 @@
               ></v-progress-circular>
           </div>
           <div v-else>
-            <v-list dense>
-              <v-list-item  v-for="(signature, index) in signatures " :key="index">
-                <v-list-item-content>
-                  <signature
-                      :signature="signature"
-                      style="width: 100%"
-                      @click:user-id="signaturesByUser"
-                  />
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-
+            <div v-if="signatures.length < 1">
+              No signatures found. Try another RepoId or UserId!
+            </div>
+            <div v-else>
+              <v-list dense>
+                <v-list-item  v-for="(signature, index) in signatures " :key="index">
+                  <v-list-item-content>
+                    <signature
+                        :signature="signature"
+                        style="width: 100%"
+                        @click:user-id="signaturesByUser"
+                    />
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </div>
           </div>
         </v-container>
     </v-content>
