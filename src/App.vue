aaf8cad2f263236a45019af59fc3e21883fb775a
fabianriewe <fabianriewe00@gmail.com>
2019-12-22 17:06:02 +0100

    feat: added search by userId

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,58 @@
 <template>
   <v-app>
     <v-content>
-        <h1>By Repo:</h1>
-        <v-text-field
-                v-model="repoId"
-        >
-        </v-text-field>
-        <v-btn @click="usersBySignature()">Search</v-btn>
-        <div v-if="loading">
-            <v-progress-circular
-                    indeterminate
-                    color="primary"
-            ></v-progress-circular>
-        </div>
-        <div v-else>
-            <user :user="user"  v-for="(user, index) in users " :key="index"></user>
-        </div>
+        <v-container>
+          <v-row>
+            <v-col><search-bar headline="By RepoId" @search="signaturesByRepo"></search-bar></v-col>
+            <v-col><search-bar headline="By UserId" @search="signaturesByUser"></search-bar></v-col>
+          </v-row>
+          <div v-if="loading">
+              <v-progress-circular
+                      indeterminate
+                      color="primary"
+              ></v-progress-circular>
+          </div>
+          <div v-else>
+              <user :user="user"  v-for="(user, index) in users " :key="index"></user>
+          </div>
+        </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import User from './components/User';
+import SearchBar from './components/SearchBar';
 
 export default {
   name: 'App',
 
   components: {
     User,
+      SearchBar,
   },
 
   data: () => ({
     users: [],
-      repoId: undefined,
       loading: false
   }),
   methods: {
-    async usersBySignature() {
-        if (this.repoId) {
+    async signaturesByRepo(repoId) {
+        if (repoId) {
             this.loading = true;
-            const response = await this.axios.get('/repos?repo_id=' + this.repoId);
+            const response = await this.axios.get('/repos?repo_id=' + repoId);
             this.users = response.data;
             this.loading = false;
         }
-    }
+    },
+      async signaturesByUser(userId) {
+          if (userId) {
+              this.loading = true;
+              const response = await this.axios.get('/users?user_id=' + userId);
+              this.users = response.data;
+              this.loading = false;
+          }
+      }
   },
   async mounted() {
   }
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>{{headline}}</h1>
+        <v-row>
+            <v-col>
+                <v-text-field
+                        v-model="input"
+                >
+                </v-text-field>
+            </v-col>
+            <v-col>
+                <v-btn @click="$emit('search', input)">Search</v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchBar",
+        props: ["headline"],
+        data: () => {
+            return {
+                input: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
